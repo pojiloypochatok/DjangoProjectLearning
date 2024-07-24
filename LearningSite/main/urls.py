6fcb021deffe02cab4a5befb892bcb60a0e32775
Dedok228 <arsensed34@gmail.com>
2024-07-24 03:09:34 +0300

    Initial commit

---
+++ LearningSite/main/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from main import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.index, name='homepage'),
+    path('courses', views.courses, name='courses')
+]
---
+++ LearningSite/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.shortcuts import render
+# Create your views here.
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def courses(request):
+    return render(request, 'main/courses.html')
