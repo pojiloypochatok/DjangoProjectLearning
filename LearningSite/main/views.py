e67ecc9105195d77cc29009ba9f818661e4139b1
jackass oldman <82373905+Dedok228@users.noreply.github.com>
2024-07-24 05:13:09 +0300

    Реформат кода

--- LearningSite/main/views.py
+++ LearningSite/main/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.shortcuts import render
+
+
 # Create your views here.
 
 def index(request):
     return render(request, 'main/index.html')
+
 
 def courses(request):
     return render(request, 'main/courses.html')
