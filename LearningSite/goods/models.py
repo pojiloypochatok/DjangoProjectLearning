6329d45ae15670c6f13ef5963994a155cd33ccb2
jackass oldman <82373905+Dedok228@users.noreply.github.com>
2024-07-31 02:50:27 +0300

    Добавил заготовку под модели, добавил иконок в курсы, настроил админу и тд

--- LearningSite/goods/models.py
+++ LearningSite/goods/models.py
@@ -1,1 +1,26 @@
 # Create your models here.
+from django.db import models
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Имя')
+    slug = models.CharField(max_length=150, unique=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Имя', )
+    slug = models.CharField(max_length=150, unique=True, null=True, verbose_name='URL')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
