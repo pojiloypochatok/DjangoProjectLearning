36f65a6a517e85462ce5ea36fccae26c35805acd
jackass oldman <82373905+Dedok228@users.noreply.github.com>
2024-08-23 02:46:04 +0300

    Добавил полнотекстовый поиск, настроил postgresql НИХУЯ НЕ ЗАГРУЖАЮТСЯ ФИКСТУРЫ, У МЕНЯ ПОДГОРЕЛА ЖОПА И Я ПОДЗАБИЛ ХУЙ ДО ЛУЧШИХ ВРЕМЕН!

--- LearningSite/LearningSite/settings.py
+++ LearningSite/LearningSite/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     'debug_toolbar',
 
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'LearningSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER': 'home',
+        'PASSWORD': 'home',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- LearningSite/goods/utils.py
+++ LearningSite/goods/utils.py
@@ -1,19 +1,14 @@
-from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery
 
 from .models import Products
 
 
 def q_search(request):
     query = request.GET.get('q')
-    if query.isdigit() and len (query) <= 5:
+    if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
-    keywords = [word for word in query.split() if len(word) >= 2]
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
 
-    q_objects = Q()
-
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-
-    return Products.objects.filter(q_objects)
+    return Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
